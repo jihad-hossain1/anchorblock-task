33065ebdc7812c0f8c69af0a7e139ac2047125ec
jihad-hossain1 <jihadkhan934@gmail.com>
2023-12-15 18:35:57 +0600

    a user signup by firebase with redux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import MainLayout from "./layouts/MainLayout";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
+import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}>
-    <MainLayout />
-  </RouterProvider>
+  <Provider store={store}>
+    <RouterProvider router={router}>
+      <MainLayout />
+    </RouterProvider>
+  </Provider>
 );
--- src/pages/Authentication/SignUp.jsx
+++ src/pages/Authentication/SignUp.jsx
@@ -1,24 +1,54 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import LoginLogo from "./LoginLogo";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { motion } from "framer-motion";
 import { framer_error } from "../../utils/fremer.motion";
 import PasswordStrengthBar from "./PasswordStrengthBar";
+import toast, { Toaster } from "react-hot-toast";
+import { createUser } from "../../redux/fetures/users/userSlice";
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const {
+    isLoading,
+    isError,
+    error,
+    email: isEmail,
+  } = useSelector((state) => state.userSlice);
+  // console.log(email);
+  console.log(isEmail?.user?.uid);
   const {
     register,
     handleSubmit,
     getValues,
     formState: { errors },
+    reset,
   } = useForm();
+
+  useEffect(() => {
+    if (isError && error) {
+      toast.error(error);
+    }
+  }, [isError, error]);
+
   const onSubmit = (data) => {
-    console.log(data);
+    const { name, email, password } = data;
+    dispatch(createUser({ name, email, password }));
+    toast.success(`${name} your account create successfull`);
+    if (name && email && password) {
+      setTimeout(() => {
+        navigate("/");
+      }, 1500);
+    }
+    reset();
   };
 
   return (
     <main className="flex justify-center items-center h-[100vh]">
+      <Toaster />
       <div className="p-6 md:p-10 border shadow-[2px_5px_15px_rgba(0,0,0,0.25)] rounded-xl flex flex-col gap-10">
         <div className="flex flex-col gap-3">
           <div className="flex items-center gap-4">
@@ -31,6 +61,27 @@ const SignUp = () => {
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col gap-5 w-full md:w-[330px] mx-auto"
         >
+          <div className="flex flex-col gap-3">
+            <label htmlFor="name">Name</label>
+            <input
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "name field is required",
+                },
+              })}
+              type="text"
+              name="name"
+              className={` ${
+                errors.name && "focus:outline-red-500 border border-red-500"
+              } cinpt`}
+            />
+            {errors.name && (
+              <motion.span {...framer_error} className="text-sm text-red-600">
+                {errors?.name.message}
+              </motion.span>
+            )}
+          </div>
           <div className="flex flex-col gap-3">
             <label htmlFor="email">Email</label>
             <input
